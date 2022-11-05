a48dce906c7cebeb1ddaa510ba3eae6dfac57bd7
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-11-05 09:17:41 -0300

    Adicionando arquivo com desenvolvimento do for para a função de inserir uma nova matriz.

--- arquivos/at_4.asm
+++ arquivos/at_4.asm
@@ -1,13 +1,14 @@
 	.data
 	
-M:	.string "\nmenu: \n1) le_matriz \n2) imprime_matriz \n3) calcula_determinante \n4) ordena_matriz \n5) maior_elemento\n0) encerrar programa\nop: "
 op:	.word
 a:	.word 1
 b:	.word 2
 c:	.word 3
 d:	.word 4
 e:	.word 5
-	
+matriz:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+M:	.string "\nmenu: \n1) le_matriz \n2) imprime_matriz \n3) calcula_determinante \n4) ordena_matriz \n5) maior_elemento\n0) encerrar programa\nop: "	
+pula:	.string "\n"
 	
 	.text
 
@@ -18,6 +19,7 @@ main:
 	lw s7, c
 	lw s8, d
 	lw s9, e
+	addi s10, s10, 0
 	
 	
 menu:
@@ -63,9 +65,46 @@ fim_z:
 	j fim_tudo
 
 func_a:
-	li a7, 4
-	la a0, a
+	
+	# imprime 
+	
+	# le inteiro
+	li a7, 5
 	ecall
+	
+	add s10, a0, zero
+	
+	# inicializando variaveis
+	add a6, zero, zero
+	
+	
+for_1:
+	bge a6, s10, fim_for_1
+	add a5, zero, zero
+for_2:
+	bge a5, s10, fim_for_2
+	
+	li a7, 1
+	mv a0, a6
+	ecall
+	mv a0, a5
+	ecall
+	li a7, 4
+	la a0, pula
+	ecall
+	
+	# atualiza iteração
+	addi a5, a5, 1
+	j for_2
+	
+fim_for_2:
+	
+	# atualiza iteração
+	addi a6, a6, 1
+	j for_1
+fim_for_1:
+	
+	
 	ret
 	
 func_b:
