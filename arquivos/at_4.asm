0c4683a92ab48e3c49e65f1c9e1e1a85e0b880a1
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-11-06 16:20:27 -0300

    Adicionando desenvovimento das funções de ordenar a matriz e calcular o determinante. Corrigindo os retornos e arrumando as string para imprimir os resultados.

--- arquivos/at_4.asm
+++ arquivos/at_4.asm
@@ -7,16 +7,19 @@ c:	.word 3
 d:	.word 4
 e:	.word 5
 matriz:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-det:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+det:	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 M:	.string "\nmenu: \n1) le_matriz \n2) imprime_matriz \n3) calcula_determinante \n4) ordena_matriz \n5) maior_elemento\n0) encerrar programa\nop: "	
 pula:	.string "\n"
 dp:	.string ": "
 dim:	.string "Digite a dimensão: "
 di: 	.string "Dimensão invalida!\n"
+dtm:	.string "Determinante da matriz é: "
+oma:	.string	"O maior numero da matriz é: "
+nl:	.string "na linha: "
+nc:	.string "na coluna: "
 	
 	.text
 
-			
 main:
 	lw	s5, a
 	lw 	s6, b
@@ -50,17 +53,77 @@ fim_a:
 fim_b:
 
 	bne 	t0, s7, fim_c
+	# verifica se a matriz possui dimensão 3
+	addi	t1, zero, 3
+	bne	s10, t1, final_c
 	jal 	func_c
 	
+	add	t1, zero, a0
+	
+	li	a7, 4
+	la	a0, dtm
+	ecall
+	li	a7, 1
+	add	a0, zero, t1
+	ecall
+	li	a7, 4
+	la	a0, pula
+	ecall
+	j	fim_c
+	
+final_c:
+	li	a7, 4
+	la	a0, di
+	ecall
+
 fim_c:
+
+
+	
+
 
 	bne 	t0, s8, fim_d
 	jal 	func_d
+
 	
 fim_d:
 
 	bne 	t0, s9, fim_e
 	jal 	func_e
+	add	t1, zero, a0
+	add	t2, zero, a1
+	add	t3, zero, a2
+	
+	# imprimir maior
+	li	a7, 4
+	la	a0, oma
+	ecall
+	li	a7, 1
+	add	a0, zero, t1
+	ecall
+	li	a7, 4
+	la	a0, pula
+	ecall
+	# imprimir linha do maior
+	li	a7, 4
+	la	a0, nl
+	ecall
+	li	a7, 1
+	add	a0, zero, t2
+	ecall
+	li	a7, 4
+	la	a0, pula
+	ecall
+	# imprimir coluna do maior
+	li	a7, 4
+	la	a0, nc
+	ecall
+	li	a7, 1
+	add	a0, zero, t3
+	ecall
+	li	a7, 4
+	la	a0, pula
+	ecall
 	
 fim_e:
 
@@ -192,59 +255,178 @@ fim_for_b_1:
 
 func_c:
 	la 	t3, matriz
-	addi	t5, zero, 3
-	addi	t6, zero, 5
+	la	t4, det
 	
-	# verifica se a matriz possui dimensão 3
-	addi	t1, zero, 3
-	bne	s10, t1, final_c
-	
-	# inicializando for_1 com 0
-	add 	a6, zero, zero
-for_c_1:
-	# condicao for_1
-	bge 	a6, t5, fim_for_c_1
-	
-	# inicializando for_2 com 0
-	add 	a5, zero, zero
-for_c_2:
-	# condicao for_2
-	bge 	a5, t6, fim_for_c_2
-	# corpo - inicio
-	
-	
-	# 0 4 8 12 16 
+	# 0  4  8  12 16 
 	# 20 24 28 32 36 
 	# 40 44 48 52 56
+	# carregando os valores da matriz 3x3 para a 3x5 para calcular o determinante
+	lw	t0, 0(t3)
+	sw	t0, 0(t4)
+	sw	t0, 12(t4)
+	
+	lw	t0, 4(t3)
+	sw	t0, 4(t4)
+	sw	t0, 16(t4)
+	
+	lw	t0, 8(t3)
+	sw	t0, 8(t4)
+	
+	lw	t0, 12(t3)
+	sw	t0, 20(t4)
+	sw	t0, 32(t4)
+	
+	lw	t0, 16(t3)
+	sw	t0, 24(t4)
+	sw	t0, 36(t4)
+	
+	lw	t0, 20(t3)
+	sw	t0, 28(t4)
+	
+	lw	t0, 24(t3)
+	sw	t0, 40(t4)
+	sw	t0, 52(t4)
+	
+	lw	t0, 28(t3)
+	sw	t0, 44(t4)
+	sw	t0, 56(t4)
+	
+	lw	t0, 32(t3)
+	sw	t0, 48(t4)
+	
+	# calculando o valor do determinante
+	
+	# zerando os valores presentes em a0, a1 e a2
+	add	a0, zero, zero
+	add	a1, zero, zero
+	add	a2, zero, zero
+	
+	# a0 = 00 * 11 * 22
+	lw	t5, 0(t4)
+	lw	t6, 24(t4)
+	mul	a0, t5, t6
+	lw	t5, 48(t4)
+	mul	a0, a0, t5
+	
+	# a1 = 01 * 12 * 23
+	lw	t5, 4(t4)
+	lw	t6, 28(t4)
+	mul	a1, t5, t6
+	lw	t5, 52(t4)
+	mul	a1, a1, t5
+	
+	# a2 = 02 * 13 * 24
+	lw	t5, 8(t4)
+	lw	t6, 32(t4)
+	mul	a2, t5, t6
+	lw	t5, 56(t4)
+	mul	a2, a2, t5
+	
+	# t0 = a0 + a1 + a2
+	add	t0, a0, a1
+	add	t0, t0, a2
+	
+	# zerando os valores presentes em a0, a1 e a2
+	add	a0, zero, zero
+	add	a1, zero, zero
+	add	a2, zero, zero
+	
+	# a0 = 02 * 11 * 20
+	lw	t5, 8(t4)
+	lw	t6, 24(t4)
+	mul	a0, t5, t6
+	lw	t5, 40(t4)
+	mul	a0, a0, t5
+	
+	# a1 = 03 * 12 * 21
+	lw	t5, 12(t4)
+	lw	t6, 28(t4)
+	mul	a1, t5, t6
+	lw	t5, 44(t4)
+	mul	a1, a1, t5
+	
+	# a2 = 04 * 13 * 22
+	lw	t5, 16(t4)
+	lw	t6, 32(t4)
+	mul	a2, t5, t6
+	lw	t5, 48(t4)
+	mul	a2, a2, t5
 	
 	
-	# corpo - fim
-	# atualiza iteração for_2
-	addi 	a5, a5, 1
-	j 	for_c_2
+	# t0 = t0 - a0 - a1 - a2
+	sub	t0, t0, a0
+	sub	t0, t0, a1
+	sub	t0, t0, a2
 	
-fim_for_c_2:
-	
-	# atualiza iteração for_1
-	addi 	a6, a6, 1
-	j 	for_c_1
-fim_for_c_1:
-
-	ret
-final_c:
-	# dimensão invalida
+	mv	a0, t0
+	# movendo 3 para o t0 apenas para não bugar o menu
+	li	t0, 3
+	# 0  4  8  12 16 
+	# 20 24 28 32 36 
+	# 40 44 48 52 56
 	ret
 
 func_d:
-	li 	a7, 4
-	la 	a0, d
-	ecall
+	la 	t0, matriz
+	
+	mul	t1, s10, s10
+	mul	t2, s10, s10
+	addi	t2, t2, -1
+	
+	# inicializando for_1 com 0
+	add 	a6, zero, zero
+for_d_1:
+	# condicao for_1
+	bge 	a6, t1, fim_for_d_1
+	
+	# inicializando for_2 com 0
+	add 	a5, zero, zero
+	mv	t3, t0
+	mv	t4, t0
+	addi	t4, t4, 4
+for_d_2:
+	# condicao for_2
+	bge 	a5, t2, fim_for_d_2
+	
+	# inicio corpo
+	
+	lw	t5, 0(t3)
+	lw	t6, 0(t4)
+	
+	ble t5, t6, n_t
+	# troca
+	sw	t6, 0(t3)
+	sw	t5, 0(t4)
+n_t:
+	
+	# atualiza atualiza valores 
+	addi	t3, t3, 4
+	addi	t4, t4, 4
+	
+	# fim corpo
+	
+	# atualiza iteração for_2
+	addi 	a5, a5, 1
+	j 	for_d_2
+	
+fim_for_d_2:
+	
+	# atualiza iteração for_1
+	addi 	a6, a6, 1
+	j 	for_d_1
+fim_for_d_1:
+	
+	
+	# para não bugar o t0
+	li	t0, 3
 	ret
 
 func_e:
 	
 	la 	t3, matriz
 	lw 	t4, 0(t3)
+	addi	a1, zero, 0
+	addi	a2, zero, 0
 	# inicializando for_1 com 0
 	add 	a6, zero, zero
 for_e_1:
@@ -261,13 +443,16 @@ for_e_2:
 	# carregando o valor lido na matriz
 	addi 	t3, t3, 4
 	lw 	a0, 0(t3)
+	# atualiza iteração for_2
+	addi 	a5, a5, 1
 	bge 	t4, a0, maior_p
 	# novo menor
 	add 	t4, zero, a0
+	add	a1, zero, a6
+	add	a2, zero, a5
 maior_p:
 	
-	# atualiza iteração for_2
-	addi 	a5, a5, 1
+	
 	j 	for_e_2
 	
 fim_for_e_2:
@@ -277,10 +462,7 @@ fim_for_e_2:
 	j 	for_e_1
 fim_for_e_1:
 
-	li 	a7, 1
 	add 	a0, zero, t4
-	ecall
-
 	ret
 	
 fim_tudo:
